04859ecc00e1d7a2ae0e90e6cc8f1ede80480303
Arnold Gandarillas Castillo <arkgast@gmail.com>
2024-01-30 09:12:15 -0400

    test: add invariant test for setNumber

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,14 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
 import {Counter} from "../src/Counter.sol";
 
-contract CounterTest is Test {
+contract CounterTest is StdInvariant, Test {
     Counter public counter;
 
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+
+        targetContract(address(counter));
     }
 
     function test_Increment() public {
@@ -20,5 +23,9 @@ contract CounterTest is Test {
     function testFuzz_SetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
+    }
+
+    function invariant_testSetNumber() public {
+        assertGe(counter.number(), 0);
     }
 }
